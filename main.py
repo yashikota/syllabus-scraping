ae1bf041642e315c58c45f9dabb4df57c43d75d9
yashikota <52403688+yashikota@users.noreply.github.com>
2024-03-18 00:54:06 +0900

    add 2024

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import syllabus
 
 
 def main():
-    YEAR: str = "2023"
+    YEAR: str = "2024"
 
-    # 学部/学科,URL,曜日,時限のリストを取得
+    # 学部/学科, URL, 曜日,時限 のリストを取得
     data: list[str] = list(syllabus.get_data(YEAR))
 
     # スクレイピング
     scraped_data: dict = syllabus.Scraping().scraper(YEAR, data)
 
-    # 通常出力
-    syllabus.output(YEAR, scraped_data)
-
-    # テーブル用の出力
+    # 一覧表示用と詳細表示用の出力
     syllabus.table_output(YEAR, scraped_data)
+    syllabus.output(YEAR, scraped_data)
 
 
 if __name__ == "__main__":
